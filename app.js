1d712ac5dce672b195bb895316de0817b4443b37
gamars99 <54536701+gamars99@users.noreply.github.com>
2020-03-11 10:56:35 +0100

    add exemple for use modele and view

---
+++ app.js
@@ -0,0 +1,24 @@
+// Initialisation du modèle
+title = Backbone.Model.extend();
+var title = new title({
+  h1titel: 'Find your ideal hotel and compare prices from different websites',
+  h3titel: 'Try searching for a city, a specific hotel, or even a landmark !'
+});
+
+//title
+$(function () {
+  var titleTrivago = _.template("<div class='col-12'><h1> <%= toto1 %> </h1></div><div class='col-12'><p><%= toto2 %></p></div>");
+ 
+  var insert = Backbone.View.extend({
+    initialize: function () {
+      this.render();
+    },
+    render: function () {
+      this.$el.html(titleTrivago({toto1: title.get('h1titel'), toto2: title.get('h3titel')}));
+    }
+  });
+  new insert({
+    el: $('.title'),
+  });
+});
+
