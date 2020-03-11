58c8080256bb99fa570837d81b38c60410e3c100
gamars99 <54536701+gamars99@users.noreply.github.com>
2020-03-11 12:07:33 +0100

    add logo with backbone

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 // Initialisation du modèle
-title = Backbone.Model.extend();
-var title = new title({
+titles = Backbone.Model.extend();
+var title = new titles({
   h1titel: 'Find your ideal hotel and compare prices from different websites',
   h3titel: 'Try searching for a city, a specific hotel, or even a landmark !'
 });
 
+LogoTrivago = Backbone.Model.extend();
+var logo = new LogoTrivago({
+  lien: 'img/logo_trivago.jpg'
+})
+
 //title
 $(function () {
   var titleTrivago = _.template("<div class='col-12'><h1> <%= toto1 %> </h1></div><div class='col-12'><p><%= toto2 %></p></div>");
- 
   var insert = Backbone.View.extend({
     initialize: function () {
       this.render();
@@ -19,6 +23,22 @@ $(function () {
   });
   new insert({
     el: $('.title'),
+  });
+});
+
+//logo
+$(function () {
+  var titleLogo = _.template("<img src='<%= lienlogo %>'>");
+  var insert = Backbone.View.extend({
+    initialize: function () {
+      this.render();
+    },
+    render: function () {
+      this.$el.html(titleLogo({lienlogo: logo.get('lien')}));
+    }
+  });
+  new insert({
+    el: $('.logoTrivago'),
   });
 });
 
