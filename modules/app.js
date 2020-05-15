7912afd0035613da615dc385ad47ad9e91970c9d
Oberson Gaël <54536800+Oberson-Gael@users.noreply.github.com>
2020-05-15 15:18:27 +0200

    add modules

---
+++ modules/app.js
@@ -0,0 +1,43 @@
+// Initialisation du modèle
+titles = Backbone.Model.extend();
+var title = new titles({
+  h1titel: 'Find your ideal hotel and compare prices from different websites',
+  h3titel: 'Try searching for a city, a specific hotel, or even a landmark !'
+});
+
+LogoTrivago = Backbone.Model.extend();
+var logo = new LogoTrivago({
+  lien: 'img/logo_trivago.jpg'
+});
+
+//title
+$(function () {
+  var titleTrivago = _.template("<div class='col-12'><h1 id='titre1'> <%= toto1 %> </h1></div><div class='col-12'><p id='titre2'><%= toto2 %></p></div>");
+  var insert = Backbone.View.extend({
+    initialize: function () {
+      this.render();
+    },
+    render: function () {
+      this.$el.html(titleTrivago({ toto1: title.get('h1titel'), toto2: title.get('h3titel') }));
+    }
+  });
+  new insert({
+    el: $('.title'),
+  });
+});
+
+//logo
+$(function () {
+  var titleLogo = _.template("<img class='w-100' src='<%= lienlogo %>'>");
+  var insert = Backbone.View.extend({
+    initialize: function () {
+      this.render();
+    },
+    render: function () {
+      this.$el.html(titleLogo({ lienlogo: logo.get('lien') }));
+    }
+  });
+  new insert({
+    el: $('.logoTrivago'),
+  });
+});
